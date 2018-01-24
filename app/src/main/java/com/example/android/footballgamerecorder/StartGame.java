af2e4a44c76fc2bdf9c733ed7f456ceb511a0ef1
TheBaileyBrew <matthew.bailey@hthackney.com>
2018-01-24 17:02:56 -0500

    Updated values

--- app/src/main/java/com/example/android/footballgamerecorder/StartGame.java
+++ app/src/main/java/com/example/android/footballgamerecorder/StartGame.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by grumnb on 1/24/2018.
@@ -12,9 +13,13 @@ import android.view.View;
 public class StartGame extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_start);
+
+        String teamOneName = getIntent().getStringExtra("team_one_name");
+        TextView teamOneText = (TextView) findViewById(R.id.team_one_game_start_name_display);
+        teamOneText.setText(teamOneName);
 
 
 
