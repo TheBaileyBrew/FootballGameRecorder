f78d170609f8e9515ecc2cc83b8b0a489b7aa5d9
TheBaileyBrew <matthew.bailey@hthackney.com>
2018-01-26 15:39:06 -0500

    Added Scoring Page (Needs onClick methods still)

--- app/src/main/java/com/example/android/footballgamerecorder/GameHasStarted.java
+++ app/src/main/java/com/example/android/footballgamerecorder/GameHasStarted.java
@@ -2,6 +2,10 @@ package com.example.android.footballgamerecorder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 /**
@@ -9,7 +13,7 @@ import android.widget.TextView;
  */
 
 public class GameHasStarted extends AppCompatActivity {
-
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +22,28 @@ public class GameHasStarted extends AppCompatActivity {
         String teamOneName = getIntent().getStringExtra("team_one");
         String teamTwoName = getIntent().getStringExtra("team_two");
 
-        TextView textView = (TextView) findViewById(R.id.team_one_name);
-        textView.setText(teamOneName);
-        textView.setText(teamTwoName);
+        TabHost host = (TabHost)findViewById(R.id.tabhost);
+        host.setup();
+
+        //Team One - Tab One
+        TabHost.TabSpec spec = host.newTabSpec("Team One");
+        spec.setContent(R.id.team_one);
+        spec.setIndicator(teamOneName);
+        host.addTab(spec);
+
+        //Team Two - Tab Two
+        spec = host.newTabSpec("Team One");
+        spec.setContent(R.id.team_two);
+        spec.setIndicator(teamTwoName);
+        host.addTab(spec);
+
+
+
 
 
     }
+
+
 
 
 }
